ffd05f1e0859b6d91906c9f246a21926ee54487d
Nicholas Wang <clone.of.snake+github@gmail.com>
2012-01-02 17:04:07 +0800

    ch 3 exercises, created Help page, base_title, added autotest to Gemfile

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,12 @@ describe PagesController do
     it "should have the right title" do
       get "home"
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
+    end
+
+    it "should not have a blank body" do
+      get "home"
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -25,7 +34,7 @@ describe PagesController do
     it "should have the right title" do
       get "contact"
       response.should have_selector("title",
-                      :content => " | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -38,8 +47,20 @@ describe PagesController do
     it "should have the right title" do
       get "about"
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get "help"
+      response.should have_selector("title",
+                                    :content => @base_title + " | Help")
+    end
+  end
 end
