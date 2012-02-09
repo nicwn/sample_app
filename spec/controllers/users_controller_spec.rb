44e3d466bfbc2a86814480164f5e4d9d4bfe8eb1
Nicholas Wang <clone.of.snake+github@gmail.com>
2012-02-09 18:25:13 +0800

    User model with passwords & user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,44 @@ require 'spec_helper'
 describe UsersController do
   render_views
 
+  describe "GET 'show" do
+
+    # 7.3.1 Testing user show page with factories p.248
+    before(:each) do
+      # in users_controller, we have: @user = User.find(params[:id])
+      # so in test we need a @user instance variable
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    # assigns(:user) is a facility provided by RSpec. The assigns method
+    # takes a symbol argument and returns the value of the corresponding 
+    # instance variable in the controller action.
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,12 @@ require 'spec_helper'
 describe User do
 
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = {
+      :name     => "Example User",
+      :email    => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -56,14 +61,84 @@ describe User do
   it "should reject duplicate email addresses" do
     # Put a user with a given email address into the database
     User.create!(@attr)    # create a db record with that user first
-    user_with_duplicate_email = User.new(@attr)    # then test making new, same user
+    user_with_duplicate_email = User.new(@attr)    # then test with new, same user
     user_with_duplicate_email.should_not be_valid
   end
 
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))    # create db record w/ upcased email
-    user_with_duplicate_email = User.new(@attr)    # then test making new user, lowercase email
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+
+  # p.231 Listing 7.1
+  describe "password validation" do
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => ""))
+      should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "shucks"))
+      should_not be_valid
+    end
+
+    it "should reject a short password" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject a long password" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encryption" do
+    # Listing 7.3 - p.234
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+    describe "has_password? method" do
+
+      it "should be true if passwords matches" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false if passwords don't matches" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+
+    describe "authentication method" do
+      # temp holder
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+
+      it "should return on user object on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
+  end
+
 end
